e312682061859e3afed59d57ccb55e647213f103
carpediem804 <33194900+carpediem804@users.noreply.github.com>
2019-07-10 11:02:41 +0900

    Add files via upload
    bfs로 푼것

---
+++ 백준-1613-역사.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <stdio.h>
+#include <queue>
+#include <vector>
+
+using namespace std;
+int n, k,s;
+int check[401];
+vector <int> v[401];
+queue <int >q;
+bool bfs(int from, int to) {
+	q.push(from);
+	check[from] = 1;
+	while (q.size() != 0) {
+		int qsize = q.size();
+		for (int i = 0; i < qsize; i++) {
+			int find = q.front();
+			//printf("%d\n", find);
+			q.pop();
+			for (int j = 0; j < v[find].size(); j++) {
+				if (check[v[find][j]] == 0) {
+					check[v[find][j]] = 1;
+					if (to == v[find][j]) { //찾는 값이랑 같다면
+						return true;
+					}
+					q.push(v[find][j]);//큐에 연결된친구들 넣기
+				}
+			}//연결된 애들만큼 ~ 
+		}
+	}
+	return false;
+}
+int main(void) {
+	scanf("%d %d", &n, &k);
+	for (int i = 1; i <= k; i++) {
+		int first, second;
+		scanf("%d %d", &first, &second);
+		v[first].push_back(second);
+	}//vector에 넣엇다 
+	scanf("%d", &s);
+	for (int i = 0; i < s; i++) {
+		for (int j = 0; j < q.size(); j++) {
+			q.pop();
+		}
+		for (int k = 1; k <= n; k++) {
+			check[k] = 0;
+		}
+		int q1, q2;
+		scanf("%d %d", &q1, &q2);
+		if (bfs(q1, q2)) { printf("-1\n"); continue; }
+		for (int j = 0; j < q.size(); j++) {
+			q.pop();
+		}
+		for (int k = 1; k <= n; k++) {
+			check[k] = 0;
+		}
+		if (bfs(q2, q1)) { printf("1\n"); continue; }
+		printf("0\n");
+		
+	}
+	return 0;
+}
