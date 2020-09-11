3c1f69bed4fd6794ecff246db9edd03b1d0ffa23
taehong_min <taehong_min@tmax.co.kr>
2020-09-11 11:09:09 +0900

    leetcode

---
+++ leetcode/Bulls and Cows.c
@@ -0,0 +1,27 @@
+#include <string>
+
+using namespace std;
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        int secret_num[10] = {0,};
+        int guess_num[10] = {0,};
+        int strike_num =0;
+        for(int i=0;i<secret.size();i++){
+        
+             secret_num[secret[i]-'0'] +=1;
+             guess_num[guess[i]-'0']+=1;
+            if(secret[i]==guess[i]){strike_num++;}
+        }
+        int ball_num = 0;
+        for(int i=0;i<10;i++){
+            ball_num += min(secret_num[i],guess_num[i]);
+        }
+        string ans ="";
+        ans +=to_string(strike_num);
+        ans += "A";
+        ans += to_string(ball_num-strike_num);
+        ans += "B";
+        return ans ;
+    }
+};
