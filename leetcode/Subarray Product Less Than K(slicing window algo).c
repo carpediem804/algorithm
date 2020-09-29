1d2b5cc919d915d2c8c9c18901034b27fc980c29
taehong_min <carpediem804@naver.com>
2020-09-29 10:08:19 +0900

    0929

---
+++ leetcode/Subarray Product Less Than K(slicing window algo).c
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        if(k==0) return 0;
+    
+    int start=0;
+    int end=0;
+    int p=1;
+    int count=0;
+    
+    while(end<nums.size())
+    {
+        p*=nums[end];
+        
+        while(start<nums.size() && p>=k)
+        {
+            p=p/nums[start];
+            start++;
+        }
+        
+        if(p<k)
+        count+=end-start+1;
+        
+        end++;
+        
+    }
+    
+    return count;
+        
+    }
+};
